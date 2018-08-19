ea91d8add475fb357d853f4de538be4848cf78f9
Deepanshu Sinha <107sinha@gmail.com>
2018-08-19 23:56:36 +0530

    all tested completed

--- exact-change/fcc-cash-register.js
+++ exact-change/fcc-cash-register.js
@@ -21,6 +21,7 @@ function checkCashRegister(price, cash, cid) {
         change = [];
     } else if (allCash) {
         status = "CLOSED";
+        change = cid;
     } else {
         status = "OPEN";
     }
@@ -153,4 +154,14 @@ let test6 = checkCashRegister(19.5, 20, [
 console.log(test6);
 
 // Solution 6
-//
+// { status: 'CLOSED',
+//   change:
+//    [ [ 'PENNY', 0.5 ],
+//      [ 'NICKEL', 0 ],
+//      [ 'DIME', 0 ],
+//      [ 'QUARTER', 0 ],
+//      [ 'ONE', 0 ],
+//      [ 'FIVE', 0 ],
+//      [ 'TEN', 0 ],
+//      [ 'TWENTY', 0 ],
+//      [ 'ONE HUNDRED', 0 ] ] }
